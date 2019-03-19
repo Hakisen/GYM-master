9fb351ea6808ce3ded5e1b72da7c271fbce100b1
Håkan Kilström <hakan.kilstrom@outlook.com>
2019-03-19 16:23:54 +0100

    Add project files.

---
+++ GYM/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using GYM.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace GYM.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public RegisterModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [Range(15,150)]
+            [Display(Name = "Age")]
+            public int Age { get; set; }
+
+            [Required]
+            [StringLength(40)]
+            [Display(Name = "Name")]
+            public string Name { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, Age = Input.Age, Name = Input.Name };
+                var resultAddUser = await _userManager.CreateAsync(user, Input.Password);
+                var resultAddRole = await _userManager.AddToRoleAsync(user, "Member");
+
+                if (resultAddUser.Succeeded && resultAddRole.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password with role Member.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { userId = user.Id, code = code },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                foreach (var error in resultAddUser.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+               
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
---
+++ GYM/Controllers/GymClassesController.cs
@@ -0,0 +1,226 @@
+﻿using GYM.Data;
+using GYM.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GYM.Controllers
+{
+    [Authorize(Roles ="Member")]
+    public class GymClassesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public GymClassesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            this.userManager = userManager;
+        }
+
+        // GET: GymClasses
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return View(await _context.GymClass.ToListAsync());
+            }
+
+            if (User.IsInRole("Member"))
+            {
+                var user = await GetCurrentUserAsync();
+                var model = await _context.GymClass.Include(a => a.AttendingMembers)
+                    .ThenInclude(u => u.ApplicationUser)
+                    .ToListAsync();
+
+                ViewData["Booked"] = model.SelectMany(m => m.AttendingMembers)
+                        .Where(u => u.ApplicationUserId == user.Id)
+                        .Select(d => new { GymId = d.GymClassId, UserId = d.ApplicationUserId })
+                        .ToDictionary(a => a.GymId, a => a.UserId);
+
+                return View(model);
+            }
+            return View(await _context.GymClass.ToListAsync());
+        }
+
+        [Authorize(Roles ="Member")]
+        public async Task<IActionResult> BookingToogle(int? id)
+        {
+            if (id == null) return NotFound();
+
+            ApplicationUser currentUser = await _context.Users.Where
+                (u => u.UserName == User.Identity.Name).FirstOrDefaultAsync();
+
+            GymClass currentClass = await _context.GymClass.Where(c => c.Id == id)
+                .Include(a => a.AttendingMembers).FirstOrDefaultAsync();
+
+            if (currentClass == null)
+            {
+                return NotFound();
+            }
+
+            ApplicationUserGymClass attending = currentClass.AttendingMembers
+                .Where(u => u.ApplicationUserId == currentUser.Id &&
+                 u.GymClassId == id).FirstOrDefault();
+
+            if (attending == null)
+            {
+                var book = new ApplicationUserGymClass()
+                {
+                    ApplicationUserId = currentUser.Id,
+                    GymClassId = (int)id
+                };
+
+                _context.UserGym.Add(book);
+                _context.SaveChanges();
+            }
+            else
+            {
+                _context.UserGym.Remove(attending);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: GymClasses/Details/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gymClass = await _context.GymClass
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gymClass == null)
+            {
+                return NotFound();
+            }
+
+            return View(gymClass);
+        }
+
+        // GET: GymClasses/Create
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GymClasses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create([Bind("Id,Name,StartTime,Durartion,De")] GymClass gymClass)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gymClass);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gymClass);
+        }
+
+        // GET: GymClasses/Edit/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gymClass = await _context.GymClass.FindAsync(id);
+            if (gymClass == null)
+            {
+                return NotFound();
+            }
+            return View(gymClass);
+        }
+
+        // POST: GymClasses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,StartTime,Durartion,De")] GymClass gymClass)
+        {
+            if (id != gymClass.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gymClass);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GymClassExists(gymClass.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gymClass);
+        }
+
+        // GET: GymClasses/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gymClass = await _context.GymClass
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gymClass == null)
+            {
+                return NotFound();
+            }
+
+            return View(gymClass);
+        }
+
+        // POST: GymClasses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gymClass = await _context.GymClass.FindAsync(id);
+            _context.GymClass.Remove(gymClass);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GymClassExists(int id)
+        {
+            return _context.GymClass.Any(e => e.Id == id);
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => userManager.GetUserAsync(HttpContext.User);
+    }
+}
